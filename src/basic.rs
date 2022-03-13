9fdff82e4d416affd05a79d484f0692d67020493
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-13 20:31:13 -0300

    Update basic.rs

--- src/basic.rs
+++ src/basic.rs
@@ -1,1 +1,41 @@
 
+// Imports
+use rust_decimal_macros::dec;
+use rust_decimal::prelude::*;
+use rayon::prelude::*;
+
+//##########################################################################################################################
+
+pub fn pow(
+    value: Decimal,
+    exp: usize
+) -> Result<Decimal, Error> {
+    Ok(
+        match exp {
+            0 => dec!(1),
+            exp => match value {
+                dec!(1) => dec!(1),
+                value => (1..=exp).par_iter()
+                    .map(|_| value)
+                    .reduce(|| dec!(1), |u, d| u * d),
+            },
+        }
+    )
+}
+
+//##########################################################################################################################
+
+pub fn fac(
+    value: usize,
+) -> Result<usize, Error> {
+    Ok(
+        match value {
+            0 => 1,
+            value => (1..=value).par_iter()
+                .reduce(|| 1, |u, d| u * d),
+        }
+    )
+}
+
+//##########################################################################################################################
+

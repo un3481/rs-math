eb3ff3dc5a25f0832e2f9ee89cd4647b94b6896a
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-12 22:15:06 -0300

    Create euler.rs

---
+++ src/euler.rs
@@ -0,0 +1,12 @@
+
+fn euler() -> Result<f64, Error> {
+    Ok(
+        (1..99).par_iter()
+            .map(|n| (2..n).par_iter()
+                .map(|x| dec!(x))
+                .reduce(|| dec!(1), |u, d| u * d)
+            )
+            .map(|x| dec!(1) / x)
+            .reduce(|| dec!(2), |u, d| u + d)
+    )
+}

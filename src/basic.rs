6e61d91d0c7ef787dd80e2bb5b3b2ae477b3ed4a
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-14 18:02:43 -0300

    implement sqrt

--- src/basic.rs
+++ src/basic.rs
@@ -40,3 +40,48 @@ pub fn fac(
 
 //##########################################################################################################################
 
+pub fn sqrt_series(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
+    Ok(
+        (1..=terms).par_iter()
+            .map(|n| [
+                || basic::pow(value, n - 1).unwrap(),
+                || basic::pow(value, n - 1).unwrap() * dec!(n)
+            ].par_iter())
+            .map(|t| t.map(|f| f()).collect())
+            .map(|t| t[0] / t[1])
+            .reduce(|| dec!(0), |u, d| u + d)
+    )
+}
+
+//##########################################################################################################################
+
+fn sqrt_prepare(
+    rsn: Decimal,
+    x: Decimal
+) -> (isize, Decimal) {
+    let (d05, d15) = (dec!(0.5), dec!(1.5));
+    let sqrt15 = consts::SQRT_OF_THREE_HALFS;
+    match true {
+        (x > d15) => dec_by2(rsn * sqrt15, x / d15),
+        (x < d05) => dec_by2(rsn / sqrt15, x * d15),
+        _ => (rsn, x),
+    }
+}
+
+//##########################################################################################################################
+
+pub fn sqrt(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
+    let (rsn, rem) = sqrt_prepare(dec!(1), value);
+    Ok(
+        sqrt_series(terms, rem)? * rsn
+    )
+}
+
+//##########################################################################################################################
+

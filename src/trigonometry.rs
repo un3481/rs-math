4c25eccb8a838b89e2e25e0ca6a07c09d7dc83ad
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-16 19:17:15 -0300

    improve match perfomance

--- src/trigonometry.rs
+++ src/trigonometry.rs
@@ -15,20 +15,21 @@ use crate::constants::{
 fn trig_prepare(
     value: Decimal
 ) -> Decimal {
-    let pi2 = dec!(2) * PI;
+    const PINEG = dec!(-1) * PI;
+    const PI2 = dec!(2) * PI;
     let mut rem = dec!(0) + value;
     rem = match true {
-        (rem > dec!(PI)) => rem - (
-            (rem / pi2).floor() * pi2
+        rem > PI => rem - (
+            (rem / PI2).floor() * PI2
         ),
-        (rem < dec!(-PI)) => rem - (
-            (rem / pi2).floor() * pi2
+        rem < PINEG => rem - (
+            (rem / PI2).floor() * PI2
         ),
         _ => rem,
     };
     match true {
-        (rem > PI) => rem - pi2,
-        (rem < (dec!(-1) * PI)) => rem + pi2,
+        rem > PI => rem - PI2,
+        rem < PINEG => rem + PI2,
         _ => rem,
     }
 }
@@ -58,14 +59,17 @@ pub fn cos(
     terms: usize,
     value: Decimal
 ) -> Result<Decimal, Error> {
+    const PINEG = dec!(-1) * PI;
+    const PIHALF = PI / dec!(2);
+    const PIHNEG = PINEG / dec!(2);
     let rem = trig_prepare(value);
     Ok(
         match rem {
-            PI * dec!(1) => dec!(-1),
-            PI / dec!(2) => dec!(0),
+            PI => dec!(-1),
+            PIHALF => dec!(0),
             dec!(0) => dec!(1),
-            PI / dec!(-2) => dec!(0),
-            PI * dec!(-1) => dec!(-1),
+            PIHNEG => dec!(0),
+            PINEG => dec!(-1),
             _ => cos_series(terms, rem)?,
         }
     )
@@ -96,14 +100,17 @@ pub fn sin(
     terms: usize,
     value: Decimal
 ) -> Result<Decimal, Error> {
+    const PINEG = dec!(-1) * PI;
+    const PIHALF = PI / dec!(2);
+    const PIHNEG = PINEG / dec!(2);
     let rem = trig_prepare(value);
     Ok(
         match rem {
-            PI * dec!(1) => dec!(0),
-            PI / dec!(2) => dec!(1),
+            PI => dec!(0),
+            PIHALF => dec!(1),
             dec!(0) => dec!(0),
-            PI / dec!(-2) => dec!(-1),
-            PI * dec!(-1) => dec!(0),
+            PIHNEG => dec!(-1),
+            PINEG => dec!(0),
             _ => sin_series(terms, rem)?,
         }
     )

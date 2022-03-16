881fa22aa673a6b66eeb10fa4848c23dbd110827
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-16 03:06:20 -0300

    Update riemann.rs

--- src/primes/riemann.rs
+++ src/primes/riemann.rs
@@ -1,1 +1,27 @@
 
+
+fn mobius(
+    n: isize
+) -> Decimal {
+    let acc: usize = 0 + n;
+    let p: usize = 0;
+    // Check 2
+    if let 0=acc%2 {
+        p = p + 1;
+        acc = acc / 2;
+        if let 0=acc%2 {return 0};
+    };
+    // Check All Primes
+    let i: usize = 3;
+    loop {
+        if (i * i) > n {break};
+        if let 0=acc%i {
+            p = p + 1;
+            acc = acc / i;
+            if let 0=acc%i {return 0};
+        };
+        i = i + 2;
+    };
+    // Return Even or Odd
+    (if let 0=p%2 {-1} else {1})
+}

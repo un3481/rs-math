88b7a6006be975f36d392053fab8ee194c6a1382
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-16 13:17:43 -0300

    Update trigonometry.rs

--- src/trigonometry.rs
+++ src/trigonometry.rs
@@ -9,42 +9,44 @@ use crate::constants::{PI};
 
 //##########################################################################################################################
 
+fn trig_prepare(
+    value: Decimal
+) -> Decimal {
+    let pi2 = dec!(2) * PI;
+    let mut rem = dec!(0) + value;
+    rem = match true {
+        (rem > dec!(PI)) => rem - (
+            (rem / pi2).floor() * pi2
+        ),
+        (rem < dec!(-PI)) => rem - (
+            (rem / pi2).floor() * pi2
+        ),
+        _ => rem,
+    };
+    match true {
+        (rem > PI) => rem - pi2,
+        (rem < (dec!(-1) * PI)) => rem + pi2,
+        _ => rem,
+    }
+}
+
+//##########################################################################################################################
+
 fn cos_series(
     terms: usize,
     value: Decimal
 ) -> Result<Decimal, Error> {
     Ok(
         (0..terms).par_iter()
-            .map(|n| (if let 0=n%2 {-1} else {1}, [
-                || basic::pow(value, (n - 1) * 2).unwrap(),
-                || basic::fac((n - 1) * 2).unwrap()
+            .map(|n| [
+                || basic::pow(value, 2 * n).unwrap(),
+                || basic::fac(2 * n).unwrap(),
+                || basic::pow(dec!(-1), n).unwrap()
             ].par_iter()))
-            .map(|(s, t)| (s, t.map(|f| f()).collect()))
-            .map(|(s, t)| (t[0] / t[1]) * dec!(s))
+            .map(|v| v.map(|f| f()).collect())
+            .map(|v| (v[0] / v[1]) * v[2])
             .reduce(|| dec!(0), |u, d| u + d)
     )
-}
-
-//##########################################################################################################################
-
-fn trig_prepare(
-    value: Decimal
-) -> Decimal {
-    let pi2 = dec!(2) * PI;
-    let mut rem = dec!(0) + value;
-    match true {
-        (rem > dec!(PI)) => {
-            rem = rem - (
-                (rem / pi2).floor() * pi2
-            );
-        },
-        (rem < dec!(-PI)) => {
-            rem = rem - (
-                (rem / pi2).floor() * pi2
-            );
-        },
-        _ => {break rem},
-    }
 }
 
 //##########################################################################################################################
@@ -54,9 +56,36 @@ pub fn cos(
     value: Decimal
 ) -> Result<Decimal, Error> {
     let rem = trig_prepare(value);
+    Ok(cos_series(terms, rem)?)
+}
+
+//##########################################################################################################################
+
+fn sin_series(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
     Ok(
-    
+        (0..terms).par_iter()
+            .map(|n| [
+                || basic::pow(value, (2 * n) + 1).unwrap(),
+                || basic::fac((2 * n) + 1).unwrap(),
+                || basic::pow(dec!(-1), n).unwrap()
+            ].par_iter()))
+            .map(|v| v.map(|f| f()).collect())
+            .map(|v| (v[0] / v[1]) * v[2])
+            .reduce(|| dec!(0), |u, d| u + d)
     )
+}
+
+//##########################################################################################################################
+
+pub fn sin(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
+    let rem = trig_prepare(value);
+    Ok(sin_series(terms, rem)?)
 }
 
 //##########################################################################################################################

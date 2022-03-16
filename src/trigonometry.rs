a4a07cc1ecbe8517107b69eaa0253713c62c5fe8
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-16 06:11:48 -0300

    Update trigonometry.rs

--- src/trigonometry.rs
+++ src/trigonometry.rs
@@ -1,15 +1,39 @@
+// Imports
+use rust_decimal_macros::dec;
+use rust_decimal::prelude::*;
+use rayon::prelude::*;
 
-cb = 0;
-    sgn = (-1);
-    for (n = 1; n <= 100; n++) {
-      sgn = -sgn;
-      bot = 1;
-      for (i = 2; i <= (2 * (n - 1)); i++) {
-        bot *= i;
-      }
-      top = 1;
-      for (i = 1; i <= (2 * (n - 1)); i++) {
-        top = top * arg[1];
-      }
-      cb = cb + (sgn * (top / bot));
-    }
+// Modules
+use crate::basic;
+use crate::constants::{PI};
+
+//##########################################################################################################################
+
+fn cos_series(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
+    Ok(
+        (0..terms).par_iter()
+            .map(|n| (if let 0=n%2 {-1} else {1}, [
+                || basic::pow(value, (n - 1) * 2).unwrap(),
+                || basic::fac((n - 1) * 2).unwrap()
+            ].par_iter()))
+            .map(|(s, t)| (s, t.map(|f| f()).collect()))
+            .map(|(s, t)| (t[0] / t[1]) * dec!(s))
+            .reduce(|| dec!(0), |u, d| u + d)
+    )
+}
+
+//##########################################################################################################################
+
+pub fn cos(
+    terms: usize,
+    value: Decimal
+) -> Result<Decimal, Error> {
+    Ok(
+    
+    )
+}
+
+//##########################################################################################################################

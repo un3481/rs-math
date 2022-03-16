43f714de4de86eaa87a314d2140aee72654bf4b1
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-16 06:02:27 -0300

    Update trigonometry.rs

--- src/trigonometry.rs
+++ src/trigonometry.rs
@@ -1,1 +1,15 @@
 
+cb = 0;
+    sgn = (-1);
+    for (n = 1; n <= 100; n++) {
+      sgn = -sgn;
+      bot = 1;
+      for (i = 2; i <= (2 * (n - 1)); i++) {
+        bot *= i;
+      }
+      top = 1;
+      for (i = 1; i <= (2 * (n - 1)); i++) {
+        top = top * arg[1];
+      }
+      cb = cb + (sgn * (top / bot));
+    }

1aefcc0b4f435f4ccd9921de6aaf1b72dff76666
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-19 11:37:26 -0300

    Update euler.rs

--- src/euler.rs
+++ src/euler.rs
@@ -13,6 +13,16 @@ use crate::constants::{
 
 //##########################################################################################################################
 
+// Constants
+const D0 = dec!(0);
+const D1 = dec!(1);
+const D2 = dec!(2);
+const D4 = dec!(4);
+const D1NEG = dec!(-1);
+const EULERINV = D1 / EULER;
+
+//##########################################################################################################################
+
 fn power_series(
     terms: usize,
     value: Decimal
@@ -25,7 +35,7 @@ fn power_series(
             ].par_iter())
             .map(|t| t.map(|f| f()).collect())
             .map(|t| t[0] / t[1])
-            .reduce(|| dec!(0), |u, d| u + d)
+            .reduce(|| D0, |u, d| u + d)
     )
 }
 
@@ -35,10 +45,6 @@ pub fn power(
     terms: usize,
     value: Decimal
 ) -> Result<Decimal, Error> {
-    const D0 = dec!(0);
-    const D1 = dec!(1);
-    const D1NEG = dec!(-1);
-    const EULERINV = D1 / EULER;
     Ok(
         match value {
             D1NEG => EULERINV,
@@ -56,16 +62,16 @@ fn ln_series(
     value: Decimal
 ) -> Result<Decimal, Error> {
     Ok(
-        dec!(1) + (
+        D1 + (
             (1..=terms).par_iter()
                 .map(|n| ([
                     || basic::pow(value - EULER, n).unwrap(),
                     || basic::pow(EULER, n).unwrap() * dec!(n),
-                    || basic::pow(dec!(-1), n + 1).unwrap()
+                    || basic::pow(D1NEG, n + 1).unwrap()
                 ].par_iter())
                 .map(|t| t.map(|f| f()).collect())
                 .map(|t| (t[0] / t[1]) * t[2])
-                .reduce(|| dec!(0), |u, d| u + d)
+                .reduce(|| D0, |u, d| u + d)
         )
     )
 }
@@ -75,16 +81,16 @@ fn ln_series(
 fn ln_prepare(
     value: Decimal
 ) -> (Decimal, Decimal) {
-    let mut rem = dec!(0) + value;
-    let mut exp = dec!(0);
+    let mut rem = value;
+    let mut exp = D0;
     loop {
         match true {
-            (rem > dec!(4)) => {
-                rem = rem / dec!(2);
+            (rem > D4) => {
+                rem = rem / D2;
                 exp = exp + LN_OF_TWO;
             },
-            (rem < dec!(2)) => {
-                rem = rem * dec!(2);
+            (rem < D2) => {
+                rem = rem * D2;
                 exp = exp - LN_OF_TWO;
             },
             _ => {break},
@@ -99,8 +105,6 @@ pub fn ln(
     terms: usize,
     value: Decimal
 ) -> Result<Decimal, Error> {
-    const D0 = dec!(0);
-    const D1 = dec!(1);
     if value <= D0 {
         panic!("cannot calc ln(x) for x <= 0");
     };

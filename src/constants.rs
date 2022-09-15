0e1c61acbda71ade45a79b438efdc68bd4417a02
rendezvous <78743473+un3481@users.noreply.github.com>
2022-09-15 01:15:34 -0300

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -21,25 +21,25 @@ const D239: Decimal = dec!(239);
 //##########################################################################################################################
 
 struct Consts {
-    pub E: Decimal,
-    pub PI: Decimal,
-    pub LN_2: Decimal,
-    pub SQRT_3DIV2: Decimal,
-    pub PI2: Decimal,
-    pub PIDIV2: Decimal,
-    pub PIDIV2N: Decimal,
-    pub PIN: Decimal
+    pub E: Box<Decimal>,
+    pub PI: Box<Decimal>,
+    pub LN_2: Box<Decimal>,
+    pub SQRT_3DIV2: Box<Decimal>,
+    pub PI2: Box<Decimal>,
+    pub PIDIV2: Box<Decimal>,
+    pub PIDIV2N: Box<Decimal>,
+    pub PIN: Box<Decimal>
 }
 
 pub static consts: Consts = Consts{
-    E: D0,
-    PI: D0,
-    LN_2: D0,
-    SQRT_3DIV2: D0,
-    PI2: D0,
-    PIDIV2: D0,
-    PIDIV2N: D0,
-    PIN: D0
+    E: Box::new(D0),
+    PI: Box::new(D0),
+    LN_2: Box::new(D0),
+    SQRT_3DIV2: Box::new(D0),
+    PI2: Box::new(D0),
+    PIDIV2: Box::new(D0),
+    PIDIV2N: Box::new(D0),
+    PIN: Box::new(D0)
 };
 
 //##########################################################################################################################
@@ -94,7 +94,7 @@ pub fn pi(
 pub fn ln_2(
     terms: usize
 ) -> Decimal {
-    let E = consts.E;
+    let E = *consts.E;
     D1 + (1..=terms).into_iter()
         .map(|n|
             pow(D1N, n + 1) * (
@@ -130,17 +130,18 @@ pub fn sqrt_3div2(
 
 //##########################################################################################################################
 
-pub fn init(
+pub unsafe fn init(
     terms: usize
 ) {
-    consts.E = euler(terms);
-    consts.PI = pi(terms);
-    consts.LN_2 = ln_2(terms);
-    consts.SQRT_3DIV2 = sqrt_3div2(terms);
-    consts.PI2 = consts.PI * D2;
-    consts.PIDIV2 = consts.PI / D2;
-    consts.PIDIV2N = consts.PIDIV2 * D1N;
-    consts.PIN = consts.PI * D1N;
+    consts.E.as_mut_ptr().write(euler(terms));
+    consts.PI.as_mut_ptr().write(pi(terms));
+    consts.LN_2.as_mut_ptr().write(ln_2(terms));
+    consts.SQRT_3DIV2.as_mut_ptr().write(sqrt_3div2(terms));
+
+    consts.PI2.as_mut_ptr().write(consts.PI * D2);
+    consts.PIDIV2.as_mut_ptr().write(consts.PI / D2);
+    consts.PIDIV2N.as_mut_ptr().write(consts.PIDIV2 * D1N);
+    consts.PIN.as_mut_ptr().write(consts.PI * D1N);
 }
 
 //##########################################################################################################################

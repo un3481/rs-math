dc636856d24cbb9c5f914be7a7b643f8fd24241e
rendezvous <78743473+un3481@users.noreply.github.com>
2023-01-14 01:01:41 -0300

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -11,28 +11,32 @@ use crate::error::Error;
 
 //##########################################################################################################################
 
+// Decimal Constants
 const D0: Decimal = dec!(0);
 const D1: Decimal = dec!(1);
 const D2: Decimal = dec!(2);
 const D4: Decimal = dec!(4);
 const D1DIV2: Decimal = dec!(0.5);
 
+// Complex Constants
 const C1: Complex = Complex{ re: D1, im: D0 };
 const C2: Complex = Complex{ re: D2, im: D0 };
 const C4: Complex = Complex{ re: D4, im: D0 };
 const CI1: Complex = Complex{ re: D0, im: D1 };
 const C1DIV2: Complex = Complex{ re: D1DIV2, im: D0 };
 
+// Iteration Terms
+const TEST_ITER: usize = 48;
+
 //##########################################################################################################################
 
 #[test]
     fn test_sqrt() -> Result<(), Error> {
-        let terms = 48;
         // Test SQRT(4)
-        let res1 = sqrt(D4, terms)?;
+        let res1 = sqrt(D4, TEST_ITER)?;
         assert_eq!(res1, D2);
         // Test POW(4, 1/2)
-        let res2 = d_pow(D4, D1DIV2, terms)?;
+        let res2 = d_pow(D4, D1DIV2, TEST_ITER)?;
         assert_eq!(res1, D2);
         // Test SQRT(4) = POW(4, 1/2)
         assert_eq!(res1, res2);
@@ -44,12 +48,11 @@ const C1DIV2: Complex = Complex{ re: D1DIV2, im: D0 };
 
 #[test]
     fn test_cc_pow() -> Result<(), Error> {
-        let terms = 48;
         // Test POW(-1, 1/2)
-        let res1 = cc_pow(-C1, C1DIV2, terms);
+        let res1 = cc_pow(-C1, C1DIV2, TEST_ITER);
         assert_eq!(res1, CI1);
         // Test POW(4, 1/2)
-        let res2 = cc_pow(C4, C1DIV2, terms);
+        let res2 = cc_pow(C4, C1DIV2, TEST_ITER);
         assert_eq!(res2, C2);
         // Return Ok
         Ok(())

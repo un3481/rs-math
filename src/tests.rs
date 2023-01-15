422e762a927e8b77c4f468c66742f00a53c7613f
rendezvous <78743473+un3481@users.noreply.github.com>
2023-01-15 16:25:33 -0300

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -4,9 +4,12 @@ use rust_decimal_macros::dec;
 use rust_decimal::prelude::*;
 
 // Modules
+use crate::constants::{ E_SQR };
+
 use crate::error::Error;
 use crate::basic::{ d_pow };
 use crate::sqrt::{ sqrt, int_sqrt };
+use crate::euler::{ exp, ln };
 
 use crate::complex::types::{ Complex };
 use crate::complex::basic::{ cc_pow };
@@ -27,8 +30,12 @@ const C4: Complex = Complex{ re: D4, im: D0 };
 const CI1: Complex = Complex{ re: D0, im: D1 };
 const C1DIV2: Complex = Complex{ re: D1DIV2, im: D0 };
 
-// Iteration Term
+// Min Decimal
+const STD_DEC: u32 = 16;
+
+// Iteration Terms
 const TEST_ITER: usize = 16;
+const TEST_ITER_LARGE: usize = 32;
 
 //##########################################################################################################################
 
@@ -38,10 +45,10 @@ fn test_sqrt() -> Result<(), Error> {
     let res1 = int_sqrt(D4)?;
     assert_eq!(res1, D2);
     // sqrt(4)
-    let res2 = sqrt(D4, TEST_ITER)?.round_dp(16);
+    let res2 = sqrt(D4, TEST_ITER)?.round_dp(STD_DEC);
     assert_eq!(res2, D2);
     // pow(4, 1/2)
-    let res3 = d_pow(D4, D1DIV2, TEST_ITER)?.round_dp(16);
+    let res3 = d_pow(D4, D1DIV2, TEST_ITER)?.round_dp(STD_DEC);
     assert_eq!(res3, D2);
     // sqrt(4) == int_sqrt(4) == pow(4, 1/2)
     assert_eq!(res1, res2);
@@ -53,10 +60,24 @@ fn test_sqrt() -> Result<(), Error> {
 //##########################################################################################################################
 
 #[test]
-fn test_cc_pow() -> Result<(), Error> {
+fn test_euler() -> Result<(), Error> {
+    // exp(2)
+    let res1 = exp(D2, TEST_ITER_LARGE)?.round_dp(STD_DEC);
+    assert_eq!(res1, E_SQR.round_dp(STD_DEC));
+    // ln(e ^ 2)
+    let res2 = ln(E_SQR, TEST_ITER)?.round_dp(STD_DEC);
+    assert_eq!(res2, D2);
+    // Return Ok
+    Ok(())
+}
+
+//##########################################################################################################################
+
+#[test]
+fn test_complex() -> Result<(), Error> {
     // cc_pow(4, 1/2)
     let mut res1 = cc_pow(C4, C1DIV2, TEST_ITER)?;
-    res1.re = res1.re.round_dp(16);
+    res1.re = res1.re.round_dp(STD_DEC);
     assert_eq!(res1, C2);
     // cc_pow(-1, 1/2)
     let res2 = cc_pow(-C1, C1DIV2, TEST_ITER)?;
